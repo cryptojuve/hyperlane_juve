73a746de61f217e82a9a8e077ee6e6c527e283c2
cryptojuve <141768377+cryptojuve@users.noreply.github.com>
2024-10-09 11:54:15 +0200

    Update links.ts

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,11 +1,11 @@
 export const links = {
-  home: 'https://www.hyperlane.xyz',
+  home: 'https://hyperlane-juve.vercel.app/z',
   explorer: 'https://explorer.hyperlane.xyz',
   discord: 'https://discord.gg/VK9ZUy3aTV',
-  github: 'https://github.com/hyperlane-xyz/hyperlane-warp-ui-template',
+  github: 'https://github.com/cryptojuve/hyperlane_juve',
   docs: 'https://docs.hyperlane.xyz',
   gasDocs: 'https://docs.hyperlane.xyz/docs/reference/hooks/interchain-gas',
   chains: 'https://docs.hyperlane.xyz/docs/resources/domains',
-  twitter: 'https://twitter.com/hyperlane',
+  twitter: 'https://x.com/cryptojuve',
   blog: 'https://medium.com/hyperlane',
 };
